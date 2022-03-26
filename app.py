c3c3111f419d879fc9bbb135d6e6c7cc8654255d
jf <jpf0628@gmail.com>
2022-03-25 18:59:20 -0700

    finihsing testing scoreword testing

--- app.py
+++ app.py
@@ -26,3 +26,27 @@ def new_game():
     game = BoggleGame()
     games[game_id] = game
     return jsonify({"gameId": game_id, "board": games[game_id].board})
+
+
+@app.post("/api/score-word")
+def score_word():
+    """checks if word is legal 
+
+    """
+    
+    response = request.json
+    breakpoint()
+    current_gameId = response["gameId"]
+    current_board = games[current_gameId]
+    
+    isWordOnBoard = current_board.check_word_on_board(response['word'])
+    isWordInList =  current_board.is_word_in_word_list(response['word'])
+    #user is sending json
+    if isWordInList and isWordOnBoard:
+        return jsonify({"result": "ok"})
+
+    if not isWordInList:
+        return jsonify({"result": "not-word"})
+
+    if not isWordOnBoard:
+        return jsonify({"result": "not-on-board"})
--- test_app.py
+++ test_app.py
@@ -2,6 +2,8 @@ from unittest import TestCase
 
 from app import app, games
 
+from boggle import BoggleGame
+
 # Make Flask errors be real errors, not HTML pages with error info
 app.config['TESTING'] = True
 
@@ -13,10 +15,13 @@ class BoggleAppTestCase(TestCase):
     """Test flask app of Boggle."""
 
     def setUp(self):
-        """Stuff to do before every test."""
-
+        """Stuff to do before every test.
+        """
         self.client = app.test_client()
         app.config['TESTING'] = True
+
+        test_board = BoggleGame(fill_letters=("B") + ("A") + ("T"))
+        print(test_board)
 
     def test_homepage(self):
         """Make sure information is in the session and HTML is displayed"""
@@ -42,3 +47,36 @@ class BoggleAppTestCase(TestCase):
             #test to see if our html contains game id and games[id].board
             #self.asssertin(new game in games)
             # write a test for this route
+
+    def test_scoring_words(self):
+        #check for word
+
+        with self.client as client:
+
+            resp = client.post("/api/new-game")
+            json_data = resp.get_json()
+
+            gameId = json_data["gameId"]
+
+            game = games[gameId]
+
+            game.board = [
+                ["C","A","T","H","P"], 
+                ["O", "X", "X","K","R"], 
+                ["X", "G", "X","O","A"], 
+                ["B","C","E","B","T"], 
+                ["E","L","T","E","B"]]
+
+            resp = client.post("/api/score-word",json={"gameId":gameId, "word":'CAT'})
+            json_data = resp.get_json()
+            self.assertEqual("ok", json_data["result"])
+
+            resp = client.post("/api/score-word",json={"gameId":gameId, "word":'HPK'})
+            json_data = resp.get_json()
+            self.assertEqual("not-word", json_data["result"])
+
+            
+            resp = client.post("/api/score-word",json={"gameId":gameId, "word":'ZZZ'})
+            json_data = resp.get_json()
+            self.assertEqual("not-on-board", json_data["result"])
+
